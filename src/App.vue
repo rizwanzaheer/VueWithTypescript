2ed7383dea7fa67300eeffe5c9541c711f9ff0cd
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2019-03-10 15:07:50 +0500

    adding home view/fixing routes & adding app template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
+    <AppNavbar/>
     <router-view/>
+    <AppFooter/>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import AppNavbar from '@/components/AppNavbar.vue';
+import AppFooter from '@/components/AppFooter.vue';
+
+@Component({
+  components: {
+    AppNavbar,
+    AppFooter,
+  },
+})
+export default class App extends Vue {}
+</script>
+
 
 <style>
 </style>
