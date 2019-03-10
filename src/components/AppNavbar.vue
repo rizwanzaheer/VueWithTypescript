426f5988d13abae7fd797dfaa3beb4e3821b2639
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2019-03-10 21:39:11 +0500

    adding nav condition

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -17,10 +17,25 @@
             <i class="ion-gear-a"></i>&nbsp;Settings
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href>Sign up</a>
+        <li class="nav-item" v-if="!username">
+          <router-link class="nav-link" to="/register">Sign up</router-link>
+        </li>
+        <li class="nav-item" v-if="!username">
+          <router-link class="nav-link" to="/login">Sign in</router-link>
         </li>
       </ul>
     </div>
   </nav>
 </template>
+<script lang="ts">
+//tslint:disable
+import { Vue, Component } from "vue-property-decorator";
+import users from "@/store/moduels/user";
+
+@Component
+export default class AppNavbar extends Vue {
+  get username() {
+    users.username;
+  }
+}
+</script>
