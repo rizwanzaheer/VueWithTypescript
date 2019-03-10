6205ad7f78185707a260b89f760c5e0bcde92a23
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2019-03-10 22:06:51 +0500

    adding article

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,53 +13,17 @@
           <div class="feed-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item">
-                <a class="nav-link disabled" href>Your Feed</a>
+                <router-link class="nav-link disabled" to="/">Your Feed</router-link>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href>Global Feed</a>
+                <router-link class="nav-link active" to="/">Global Feed</router-link>
               </li>
             </ul>
           </div>
-
-          <div class="article-preview">
-            <div class="article-meta">
-              <a href="profile.html">
-                <img src="http://i.imgur.com/Qr71crq.jpg">
-              </a>
-              <div class="info">
-                <a href class="author">Eric Simons</a>
-                <span class="date">January 20th</span>
-              </div>
-              <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                <i class="ion-heart"></i> 29
-              </button>
-            </div>
-            <a href class="preview-link">
-              <h1>How to build webapps that scale</h1>
-              <p>This is the description for the post.</p>
-              <span>Read more...</span>
-            </a>
-          </div>
-
-          <div class="article-preview">
-            <div class="article-meta">
-              <a href="profile.html">
-                <img src="http://i.imgur.com/N4VcUeJ.jpg">
-              </a>
-              <div class="info">
-                <a href class="author">Albert Pai</a>
-                <span class="date">January 20th</span>
-              </div>
-              <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                <i class="ion-heart"></i> 32
-              </button>
-            </div>
-            <a href class="preview-link">
-              <h1>The song you won't ever stop singing. No matter how hard you try.</h1>
-              <p>This is the description for the post.</p>
-              <span>Read more...</span>
-            </a>
-          </div>
+          <ArticlePreview/>
+          <ArticlePreview/>
+          <ArticlePreview/>
+          <ArticlePreview/>
         </div>
 
         <div class="col-md-3">
@@ -82,3 +46,16 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+//tslint:disable
+import { Vue, Component } from "vue-property-decorator";
+import ArticlePreview from "@/components/article/ArticlePreview.vue";
+
+@Component({
+  components: {
+    ArticlePreview
+  }
+})
+export default class Home extends Vue {}
+</script>
